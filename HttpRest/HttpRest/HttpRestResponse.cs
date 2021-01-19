f6c467cae85faf3cff820fcd96351ee74819df8b
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-01-19 19:30:45 +0200

    Added HttpRestResponse

---
+++ HttpRest/HttpRest/HttpRestResponse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+namespace HttpRest
+{
+    public interface IHttpRestResponse
+    {
+        RestResult RestResult { get; }
+
+        HttpStatusCode StatusCode { get; }
+
+        Exception? InnerException { get; }
+    }
+
+    public interface IHttpRestResponse<out T> : IHttpRestResponse
+    {
+        T? Content { get; }
+    }
+
+    public sealed class RestResponse<T> : IHttpRestResponse<T>
+    {
+        public RestResult RestResult { get; }
+
+        public HttpStatusCode StatusCode { get; }
+
+        public Exception? InnerException { get; }
+
+        public T? Content { get; }
+
+        public RestResponse(RestResult restResult, HttpStatusCode statusCode, Exception? innerException, T? content)
+        {
+            RestResult = restResult;
+            StatusCode = statusCode;
+            InnerException = innerException;
+            Content = content;
+        }
+    }
+
+    public static class HttpResponseExtensions
+    {
+        public static bool IsSuccess(this IRestResponse response) => response.RestResult == RestResult.Success;
+    }
+}
