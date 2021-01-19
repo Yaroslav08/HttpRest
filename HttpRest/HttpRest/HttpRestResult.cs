6752f4383320c6fe1dd23cce8e53a7f8286272b6
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-01-19 19:28:27 +0200

    Define HttpResult enum

---
+++ HttpRest/HttpRest/HttpRestResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace HttpRest
+{
+    public enum HttpRestResult
+    {
+        Success,
+        Cancel,
+        RequestError,
+        HttpError,
+        SerializeError,
+        Unknown
+    }
+}
